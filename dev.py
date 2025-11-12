6267cf7bddff60dfafaa49759277eb0a243e1a39
Masony817 <masony817@gmail.com>
2025-11-12 00:10:17 -0500

    added LayerNorm and got val loss of 2.067

--- dev.py
+++ dev.py
@@ -122,10 +122,13 @@ class Block(nn.Module):
         head_size = n_embd // n_head
         self.sa = MultiHeadAttention(n_head, head_size)
         self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
 
     def forward(self, x):
-        x = x + self.sa(x)
-        x = x + self.ffwd(x)
+        #pre-norm variant (per-token)
+        x = x + self.sa(self.ln1(x)) 
+        x = x + self.ffwd(self.ln2(x))
         return x
 
 #overly simple bigram model
@@ -140,6 +143,7 @@ class BigramLanguageModel(nn.Module):
             Block(n_embd, n_head=4),
             Block(n_embd, n_head=4),
             Block(n_embd, n_head=4),
+            nn.LayerNorm(n_embd),
         )
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
